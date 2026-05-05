11578f77e6f57bccdf64c533f5429853cc1f7651
Imorou TOURE <sandda9gmail.com>
2026-05-05 17:47:10 +0100

    fix: align VPC reference in security.tf

--- security.tf
+++ security.tf
@@ -3,10 +3,9 @@
 resource "aws_security_group" "db_sg" {
   name        = "fluxio-db-sg"
   description = "Allow inbound PostgreSQL traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = aws_vpc.main_vpc.id # Corrigé pour correspondre à main_vpc
 
   # Inbound rule: Allow PostgreSQL (5432)
-  # Replace 0.0.0.0/0 with your specific IP for better security later
   ingress {
     from_port   = 5432
     to_port     = 5432
